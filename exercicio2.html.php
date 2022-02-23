f7410385dc7b5f2c3a747b2e8c1834b7a4af059c
Malakay-afk <mikelrapsolo@gmail.com>
2022-02-23 20:11:30 +0100

    exercicio 2

--- exercicio2.html.php
+++ exercicio2.html.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercicios 16-18</title>
+    <title>Exercicios 2</title>
 </head>
 <body>
     <h1>Multiplos del 5</h1>
     <?php
-    $limite=100;
-        for ($i=5; $i <= $limite ; $i+=5) { 
-            echo "$i<br>";
-        }
+    while ($i < 100) {
+         $i=$i+5;
+         echo "$i<br>";
+    }
     ?>
 </body>
 </html>
