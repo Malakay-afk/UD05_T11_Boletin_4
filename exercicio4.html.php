6cb559fcc390e15c7ae185b5162b6be3483ccf4c
Malakay-afk <mikelrapsolo@gmail.com>
2022-02-23 20:29:39 +0100

    exercicio 4

---
+++ exercicio4.html.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 1</title>
+</head>
+<body>
+    <h1>desde el 360 al 160</h1>
+    <?php
+    $limite=160;
+        for ($i=360; $i >= $limite ; $i=$i-20) { 
+            echo "$i<br>";
+        }
+    ?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
