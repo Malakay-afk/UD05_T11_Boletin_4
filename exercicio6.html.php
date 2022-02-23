0dd32d5b29c185d335b3c02d5cd24f9f14f1e2f4
Malakay-afk <mikelrapsolo@gmail.com>
2022-02-23 20:54:59 +0100

    ·

--- exercicio6.html.php
+++ exercicio6.html.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercicio 5</title>
+    <title>Exercicio 6</title>
 </head>
 <body>
     <h1>desde el 360 al 160</h1>
     <?php
-    $i=380;
-    while ($i > 160) {
-         $i=$i-20;
-         echo "$i<br>";
-    }
+        do {
+            $i=380;
+            $i=$i-20;
+            echo "$i<br>";
+        } while ($i > 160);
     ?>
 </body>
 </html>
