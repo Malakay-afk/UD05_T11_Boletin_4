c18fa068b062e43a3236baad4571e94cd9766271
Malakay-afk <mikelrapsolo@gmail.com>
2022-03-02 19:50:19 +0100

    exercicio 13

--- Exercicio13.html.php
+++ Exercicio13.html.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +9,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 13</title>
 </head>
-  <body>
-  </body>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
+    <label for="numero">Introduce un numero</label><br>
+    <input type="number" name="numero"> <br>
+    <input type="submit" name="Enviar">
+    </form>
+    <?php
+
+        $positivos = 0;
+        $negativos = 0;
+        $numero = $_REQUEST['numero'];
+
+        if(isset($_REQUEST['numero'])
+        && filter_var($_REQUEST['numero'],FILTER_VALIDATE_INT) && $_REQUEST['numero']>=0){
+
+            for ($i=0; $i < count($_REQUEST['numero']) ; $i++) { 
+                echo "<p>O reconto de positivos é: $i</p>";
+            }
+
+
+        }else if (isset($_REQUEST['numero'])
+        && filter_var($_REQUEST['numero'],FILTER_VALIDATE_INT) && $_REQUEST['numero']<0)
+        {
+        }    
+    ?>
+</body>
 </html>
